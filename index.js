247334b7f4b9d7b5f8378a8bc2c97189fb272092
Fatuma Ibrahim <fatmazahraabdulrashid@gmail.com>
2024-03-30 23:40:59 +0300

    lab complete

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+const divide = (parameter1, parameter2) => {
+    if (parameter2 === 0) {
+        console.log("Cannot divide by zero.");
+        return NaN;
+    }
+    return parameter1 / parameter2;
+}
+
+const result = divide(2000, 100);
+
+const square = (num) => num * num;
+
+const add = (num3, num4) => num3 + num4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
